8576841e681c25058e140b2d8ef62516150df427
guvarallo <gustavo.varallo@live.com>
2020-09-25 12:19:16 -0300

    removed service worker

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
+
 import './index.css';
 import App from './containers/App';
-import * as serviceWorker from './serviceWorker';
 import { searchRobots, requestRobots } from './reducers.js';
 import 'tachyons';
 
@@ -22,8 +22,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
