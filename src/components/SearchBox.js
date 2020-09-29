99a02e336f0c02c36e86abcd3274e1e17f969920
guvarallo <gustavo.varallo@live.com>
2020-09-29 19:49:14 -0300

    a few accessibility changes

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const SearchBox = ({searchChange}) => {
+const SearchBox = ({ searchChange }) => {
   return (
     <div className='pa2'>
-      <input 
+      <input
+        aria-label='Search Robots'
         className='pa3 ba b--green bg-lightest-blue'
-        type='search' 
+        type='search'
         placeholder='Search robots'
         onChange={searchChange}
       />
