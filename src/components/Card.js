d7d7de4a2bae609d0e54f0f61f9721deb8a7607b
guvarallo <gustavo.varallo@live.com>
2020-10-01 19:08:42 -0300

    added enzyme tests

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,17 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
 const Card = ({ firstName, lastName, email, id }) => {
   return (
-    <Fragment>
-      <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-        <img src={`https://robohash.org/${id}?size=200x200`} alt="robots"/>
-        <h2>{firstName} {lastName}</h2>
+    <>
+      <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+        <img src={`https://robohash.org/${id}?size=200x200`} alt="robots" />
+        <h2>
+          {firstName} {lastName}
+        </h2>
         <p>{email}</p>
       </div>
-    </Fragment>
+    </>
   );
-}
+};
 
 export default Card;
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,53 +2,59 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 
 import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox'
+import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 import { setSearchField, requestRobots } from '../actions/actions';
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     searchfield: state.searchRobots.searchfield,
     robots: state.requestRobots.robots,
     isPending: state.requestRobots.isPending,
-    error: state.requestRobots.error
-  }
-}
+    error: state.requestRobots.error,
+  };
+};
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
-    onSearchChange: event => dispatch(setSearchField(event.target.value)),
-    onRequestRobots: () => dispatch(requestRobots())
-  }
-}
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots()),
+  };
+};
 
 const App = (props) => {
-  const { searchfield, onSearchChange, onRequestRobots, robots, isPending } = props;
-  const filteredRobots = robots.filter(robot => {
+  const {
+    searchfield,
+    onSearchChange,
+    onRequestRobots,
+    robots,
+    isPending,
+  } = props;
+  const filteredRobots = robots.filter((robot) => {
     return robot.firstName.toLowerCase().includes(searchfield.toLowerCase());
-  })
+  });
 
   useEffect(() => {
     onRequestRobots();
     // eslint-disable-next-line
   }, []);
 
-  return isPending ?
-    <h1 style={{ textAlign: "center" }}>Loading</h1> :
-    (
-      <div className='tc'>
-        <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange={onSearchChange} /><br />
-        <Scroll>
-          <ErrorBoundary>
-            <CardList robots={filteredRobots} />
-          </ErrorBoundary>
-        </Scroll>
-      </div>
-    );
-}
-
+  return isPending ? (
+    <h1 style={{ textAlign: 'center' }}>Loading</h1>
+  ) : (
+    <div className="tc">
+      <h1 className="f1">RoboFriends</h1>
+      <SearchBox searchChange={onSearchChange} />
+      <br />
+      <Scroll>
+        <ErrorBoundary>
+          <CardList robots={filteredRobots} />
+        </ErrorBoundary>
+      </Scroll>
+    </div>
+  );
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
